55b353279a2cc70984eb5d315e51c28c5cf50cbb
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2023-07-11 18:27:45 +0700

    Data film

--- RentalFilm_C19/Form6.Designer.cs
+++ RentalFilm_C19/Form6.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RentalFilm_C19
 {
-    partial class Form5
+    partial class Form6
     {
         /// <summary>
         /// Required designer variable.
@@ -51,68 +51,68 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Sitka Banner", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(82, 73);
+            this.label1.Location = new System.Drawing.Point(79, 76);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 26);
+            this.label1.Size = new System.Drawing.Size(77, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Id pegawai";
+            this.label1.Text = "Kode film";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Sitka Banner", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(80, 124);
+            this.label2.Location = new System.Drawing.Point(79, 123);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 26);
+            this.label2.Size = new System.Drawing.Size(79, 26);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Nama pegawai";
+            this.label2.Text = "Judul film";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Sitka Banner", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(82, 178);
+            this.label3.Location = new System.Drawing.Point(79, 170);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 26);
+            this.label3.Size = new System.Drawing.Size(56, 26);
             this.label3.TabIndex = 2;
-            this.label3.Text = "No telepon";
+            this.label3.Text = "Rating";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Sitka Banner", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(82, 230);
+            this.label4.Location = new System.Drawing.Point(79, 223);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 26);
+            this.label4.Size = new System.Drawing.Size(76, 26);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Kode rental";
+            this.label4.Text = "Deskripsi";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(224, 77);
+            this.textBox1.Location = new System.Drawing.Point(192, 80);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(274, 22);
+            this.textBox1.Size = new System.Drawing.Size(279, 22);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(224, 128);
+            this.textBox2.Location = new System.Drawing.Point(192, 123);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(274, 22);
+            this.textBox2.Size = new System.Drawing.Size(279, 22);
             this.textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(223, 180);
+            this.textBox3.Location = new System.Drawing.Point(192, 170);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(274, 22);
+            this.textBox3.Size = new System.Drawing.Size(279, 22);
             this.textBox3.TabIndex = 6;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(224, 235);
+            this.textBox4.Location = new System.Drawing.Point(192, 223);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(274, 22);
+            this.textBox4.Size = new System.Drawing.Size(279, 22);
             this.textBox4.TabIndex = 7;
             // 
             // dataGridView1
@@ -123,37 +123,37 @@
             this.Column2,
             this.Column3,
             this.Column4});
-            this.dataGridView1.Location = new System.Drawing.Point(87, 337);
+            this.dataGridView1.Location = new System.Drawing.Point(84, 325);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(553, 67);
-            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.Size = new System.Drawing.Size(550, 75);
+            this.dataGridView1.TabIndex = 8;
             // 
             // Column1
             // 
-            this.Column1.HeaderText = "Id pegawai";
+            this.Column1.HeaderText = "Kode film";
             this.Column1.MinimumWidth = 6;
             this.Column1.Name = "Column1";
             this.Column1.Width = 125;
             // 
             // Column2
             // 
-            this.Column2.HeaderText = "Nama pegawai";
+            this.Column2.HeaderText = "Judul film";
             this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
             this.Column2.Width = 125;
             // 
             // Column3
             // 
-            this.Column3.HeaderText = "No telepon ";
+            this.Column3.HeaderText = "Rating";
             this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
             this.Column3.Width = 125;
             // 
             // Column4
             // 
-            this.Column4.HeaderText = "Kode rental";
+            this.Column4.HeaderText = "Deskripsi";
             this.Column4.MinimumWidth = 6;
             this.Column4.Name = "Column4";
             this.Column4.Width = 125;
@@ -161,34 +161,34 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.button1.Location = new System.Drawing.Point(523, 77);
+            this.button1.Location = new System.Drawing.Point(496, 75);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 33);
-            this.button1.TabIndex = 11;
+            this.button1.TabIndex = 9;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.button2.Location = new System.Drawing.Point(304, 280);
+            this.button2.Location = new System.Drawing.Point(273, 261);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 33);
-            this.button2.TabIndex = 12;
+            this.button2.TabIndex = 10;
             this.button2.Text = "Delete";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.button3.Location = new System.Drawing.Point(223, 280);
+            this.button3.Location = new System.Drawing.Point(192, 260);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 33);
-            this.button3.TabIndex = 13;
+            this.button3.TabIndex = 11;
             this.button3.Text = "Refresh";
             this.button3.UseVisualStyleBackColor = false;
             // 
-            // Form5
+            // Form6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -206,8 +206,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form5";
-            this.Text = "Form5";
+            this.Name = "Form6";
+            this.Text = "Form6";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
